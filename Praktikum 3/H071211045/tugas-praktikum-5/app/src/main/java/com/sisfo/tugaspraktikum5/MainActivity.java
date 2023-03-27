24bb68be19050ff0ab7d2e0bb9081e2eaffb7ea1
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-03-27 11:51:53 +0800

    add `ScoreActivity.java` and its layout

--- Praktikum 3/H071211045/tugas-praktikum-5/app/src/main/java/com/sisfo/tugaspraktikum5/MainActivity.java
+++ Praktikum 3/H071211045/tugas-praktikum-5/app/src/main/java/com/sisfo/tugaspraktikum5/MainActivity.java
@@ -2,13 +2,33 @@ package com.sisfo.tugaspraktikum5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startButton;
+    EditText nameInput;
+    ImageView profilePictureInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profilePictureInput = findViewById(R.id.profile_picture_input);
+        nameInput = findViewById(R.id.name_input);
+        startButton = findViewById(R.id.start_button);
+
+        startButton.setOnClickListener(v -> {
+            String name = nameInput.getText().toString();
+            String profilePicture = profilePictureInput.toString();
+            Intent intent = new Intent(this, ScoreActivity.class);
+//            intent.putExtra("name", name);
+//            intent.putExtra("profilePicture", profilePicture);
+            startActivity(intent);
+        });
     }
 }
